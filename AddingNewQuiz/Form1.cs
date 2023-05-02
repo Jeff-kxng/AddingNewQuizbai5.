1c41155f9228f848ed98d78236f7785f404f48a9
JeffKxng <100309426+Jeff-kxng@users.noreply.github.com>
2023-05-02 23:42:11 +0700

    Update Form1.cs

--- AddingNewQuiz/Form1.cs
+++ AddingNewQuiz/Form1.cs
@@ -29,6 +29,25 @@ namespace AddingNewQuiz
             Controls.Add(AddNewQuizForm_ClosethequizLbl);
             Controls.Add(AddNewQuizForm_Timelimit);
             Controls.Add(AddNewQuizForm_Timeexpires);
+            Controls.Add(AddNewQuizForm_dateopenCbo);
+            Controls.Add(AddNewQuizForm_MonthopenCbo);
+            Controls.Add(AddNewQuizForm_YearopenCbo);
+            Controls.Add(AddNewQuizForm_HouropenCbo);
+            Controls.Add(AddNewQuizForm_MinuteopenCbo);
+            Controls.Add(AddNewQuizForm_openTbt);
+            Controls.Add(AddNewQuizForm_openLbl);
+            Controls.Add(AddNewQuizForm_dateCloseCbo);
+            Controls.Add(AddNewQuizForm_MonthCloseCbo);
+            Controls.Add(AddNewQuizForm_YearCloseCbo);
+            Controls.Add(AddNewQuizForm_HourCloseCbo);
+            Controls.Add(AddNewQuizForm_MinuteCloseCbo);
+            Controls.Add(AddNewQuizForm_CloseTbt);
+            Controls.Add(AddNewQuizForm_CloseLbl);
+            Controls.Add(AddNewQuizForm_TimelimitTtb);
+            Controls.Add(AddNewQuizForm_TimelimiMinuteCbo);
+            Controls.Add(AddNewQuizForm_TimelimitTbt);
+            Controls.Add(AddNewQuizForm_TimelimitLbl);
+            Controls.Add(AddNewQuizForm_TimeexpiresCloseCbo);
         }
     }
 }
